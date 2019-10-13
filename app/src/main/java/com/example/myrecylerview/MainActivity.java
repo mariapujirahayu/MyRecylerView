6675205879ed4b1e1439de3bcb3d01cd0dd756c1
mariapujirahayu <mariapujir@gmail.com>
2019-10-13 23:14:50 +0700

    Menambahkan resource directory menu yang ada 3 titik di pojok kanan judul atas

--- app/src/main/java/com/example/myrecylerview/MainActivity.java
+++ app/src/main/java/com/example/myrecylerview/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.myrecylerview.adapter.ListHeroAdapter;
 import com.example.myrecylerview.model.Hero;
@@ -32,5 +34,28 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
         rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.action_list:
+                break;
+            case R.id.action_grid:
+                break;
+            case R.id.action_cardview:
+                break;
+        }
     }
 }
